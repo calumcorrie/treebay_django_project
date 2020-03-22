7f160608474e379027754cd0d46da0dbb1f1b50f
Sara Kutkova <2406155k@student.gla.ac.uk>
2020-03-22 15:39:06 +0100

    refactoring populate script

--- populate_treebay.py
+++ populate_treebay.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'treebay_django_project.settings')
@@ -11,110 +12,18 @@ from django.contrib.auth.models import User
 
 
 def populate():
-    plants_alice = [{'name': 'Spiky boi', 'description': 'This lovely cactus needs a new home, water once in a while, '
-                                                         'you can\'t kill it anyway.',
-                     'location': 'Maryhill Road, Glasgow',
-                     'categories': ['houseplants', 'succulent', 'cactus', 'allergy friendly'],
-                     'price': 5},
-                    {'name': 'Pink orchid', 'description': 'Extremely needy, having other orchid is recommended',
-                     'location': 'Maryhill Road, Glasgow',
-                     'categories': ['houseplants', 'orchid'],
-                     'price': 20}
-                    ]
 
-    plants_tom = [{'name': 'Peace lily', 'description': 'This tropical shade-loving plant helps cleanse the air in '
-                                                        'your room',
-                   'location': 'Great Western Road, Glasgow',
-                   'categories': ['houseplants', 'allergy friendly'],
-                   'price': 0},
-                  {'name': 'White orchid', 'description': 'Extremely needy, having other orchid is recommended',
-                   'location': 'Great Western Road, Glasgow',
-                   'categories': ['houseplants', 'orchid'],
-                   'price': 15},
-                  {'name': 'Aloe Vera', 'description': 'My best friend since freshman year. Needs a new loving family.',
-                   'location': 'Great Western Road, Glasgow',
-                   'categories': ['houseplants', 'succulent', 'allergy friendly'],
-                   'price': 15}
-                  ]
+    # all plants
+    plants = pd.read_excel("treebay_testing_data/plants.xlsx", index_col=0)
+    # all users
+    users = pd.read_excel("treebay_testing_data/users.xlsx", index_col=0).to_dict("index")
 
-    plants_catherine = [{'name': 'Mini pine tree', 'description': 'Brought this lovely tree from a trip to Portugal. '
-                                                                  'You must plant it outside, needs shade and sour '
-                                                                  'soil.',
-                         'location': 'Vincent Street, Glasgow',
-                         'categories': ['outdoor', 'evergreen'],
-                         'price': 10},
-                        ]
+    # add plants to users
+    for username in plants.index.unique():
+        users[username]['plants'] = plants.loc[plants.index == username].to_dict(orient='records')
 
-    plants_david = [{'name': 'Mint pot', 'description': 'Couple of leaves gone because of an occasional mojito, '
-                                                        'otherwise in pretty good shape. Need replanting in a bigger '
-                                                        'pot',
-                     'location': 'Buchanan Street, Glasgow',
-                     'categories': ['houseplants', 'herbs', 'medical'],
-                     'price': 10},
-                    {'name': 'Tulips', 'description': 'Make your garden blossom next year with some lovely tulips',
-                     'location': 'Buchanan Street, Glasgow',
-                     'categories': ['houseplants', 'outdoor', 'seasonal', 'seed plants'],
-                     'price': 2}
-                    ]
-
-    plants_liza = [{'name': 'Ficus', 'description': 'Grows quite slowly. Avoid moving it around the house, unless you '
-                                                    'want to upset it.',
-                    'location': 'Byres Road, Glasgow',
-                    'categories': ['houseplants'],
-                    'price': 12},
-                   {'name': 'Mini red rose pot', 'description': 'The little roses need a lot of sun.',
-                    'location': 'Byres Road, Glasgow',
-                    'categories': ['houseplants', 'allergy friendly'],
-                    'price': 8},
-                   {'name': 'Purple kalanchoe', 'description': 'It has been alive for 3 months without water now.',
-                    'location': 'Byres Road, Glasgow',
-                    'categories': ['houseplants', 'allergy friendly'],
-                    'price': 8}
-                   ]
-
-    plants_ben = [{'name': 'Anthurium Flamingo Flower', 'description': 'The plant would prefer to be in bright light, '
-                                                                       'moist air and well-drained yet moist soil.',
-                   'location': 'Shuna Street, Glasgow',
-                   'categories': ['houseplants', 'seasonal'],
-                   'price': 7},
-                  {'name': 'Mini palm', 'description': 'Stole it from a garden in Turkey. No idea how to take care of '
-                                                       'it.',
-                   'location': 'Shuna Street, Glasgow',
-                   'categories': ['outdoor'],
-                   'price': 6},
-                  {'name': 'Borage/Starflower', 'description': 'Needs sun and little water. Blossoms can be used for '
-                                                               'cake decoration.',
-                   'location': 'Shuna Street, Glasgow',
-                   'categories': ['houseplants', 'allergy friendly', 'edible'],
-                   'price': 18},
-                  ]
-
-    users = {
-        'andrea': {'email': 'andrea@gmail.com', 'password': 'andrea1234'},
-        'mark': {'email': 'mark@gmail.com', 'password': 'mark1234'},
-        'david': {'email': 'david@gmail.com', 'password': 'david1234', 'plants': plants_david},
-        'catherine': {'email': 'catherine@gmail.com', 'password': 'catherine1234', 'plants': plants_catherine},
-        'alice': {'email': 'alice@gmail.com', 'password': 'alice1234', 'plants': plants_alice},
-        'tom': {'email': 'tom@gmail.com', 'password': 'tom1234', 'plants': plants_tom},
-        'elizabeth': {'email': 'liza@gmail.com', 'password': 'liza1234', 'plants': plants_liza},
-        'ben': {'email': 'ben@gmail.com', 'password': 'ben1234', 'plants': plants_ben},
-    }
-
-    categories = {'houseplants': {'description': 'Plants that enjoy house music'},
-                  'allergy friendly': {
-                      'description': 'These plants have very little pollen and will never trigger you'},
-                  'outdoor': {'description': 'Plants that enjoy house music'},
-                  'evergreen': {'description': 'Its green. Ever green.'},
-                  'seasonal': {'description': 'Sadly dead by October'},
-                  'seed plants': {'description': 'More like self colonising'},
-                  'edible': {'description': 'For the japanese jelly cakes'},
-                  'medical': {'description': 'For the brownies'},
-                  'succulent': {'description': 'The family of that Aloe you killed for face moisturizer'},
-                  'herbs': {'description': 'Herbal tea or mojito? Your call!'},
-                  'cactus': {'description': 'Has been lonely in the desert for so long, kinda stubborn, but a good company'},
-                  'orchid': {'description': 'If you want to adopt one of these beautiful creatures, keep in mind that '
-                                            'they want as much attention as a woman '},
-                  }
+    # all categories
+    categories = pd.read_excel("treebay_testing_data/categories.xlsx", index_col=0).to_dict("index")
 
     # clear all users except for staff
     delete_users()
