a65901ee5cdeb0feb6b71c9d90a9a43aa75ca004
Sara Kutkova <2406155k@student.gla.ac.uk>
2020-03-23 11:15:00 +0100

    Bug fixed - list were read as string when reading from excel

--- populate_treebay.py
+++ populate_treebay.py
@@ -15,6 +15,7 @@ def populate():
 
     # all plants
     plants = pd.read_excel("treebay_testing_data/plants.xlsx", index_col=0)
+    plants['categories'] = plants.categories.apply(lambda x: x.split(', '))
     # all users
     users = pd.read_excel("treebay_testing_data/users.xlsx", index_col=0).to_dict("index")
 
