c2616e4a103dc1a6739512b10f25a041d27ae605
Calum Corrie <2393272c@student.gla.ac.uk>
2020-04-03 18:24:16 +0100

    JS Fix

--- static/js/largebutton.js
+++ static/js/largebutton.js
@@ -1,4 +1,6 @@
-$("div.largebutton").click( function(e){
-	/* Expands function of a to whole button */
-	window.location.href = $(this).children("a").attr("href");
+$(document).ready( function() {
+	$("div.largebutton").click( function(){
+		/* Expands function of a to whole button */
+		window.location.href = $(this).children("a").attr("href");
+	});
 });
